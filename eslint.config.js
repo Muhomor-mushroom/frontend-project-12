5ad25fd565bf4badf937c9977a56125e9407b033
AX <ax@MacBook-Pro-NS.local>
2025-06-01 20:03:53 +0300

    Second step, V1.0

--- eslint.config.js
+++ eslint.config.js
@@ -4,7 +4,7 @@ import { defineConfig } from 'eslint/config'
 import stylistic from '@stylistic/eslint-plugin'
 
 export default defineConfig([stylistic.configs.recommended, {
-  ignores: ['dist', 'node_modules'],
+  ignores: ['./frontend/dist', './frontend/node_modules', './frontend/build'],
 },
 { files: ['**/*.{js,mjs,cjs}'], plugins: { js }, extends: ['js/recommended'] },
 { files: ['**/*.{js,mjs,cjs}'], languageOptions: { globals: globals.browser } },
