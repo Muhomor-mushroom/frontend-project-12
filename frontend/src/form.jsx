1f232cece6ca9ed3ba54ee5a9ed4ab65b0f1f078
AX <ax@MacBook-Pro-NS.local>
2025-06-18 12:37:16 +0300

    pre-final V 1.0

--- frontend/src/form.jsx
+++ frontend/src/form.jsx
@@ -1,8 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./routes/pages/HomePage.jsx";
-import LoginPage from "./routes/pages/loginPage.jsx";
-import ErrorPage from "./routes/pages/errorPage.jsx";
-import SignupPage from "./routes/pages/signupPage.jsx";
+import LoginPage from "./routes/pages/LoginPage.jsx";
+import ErrorPage from "./routes/pages/ErrorPage.jsx";
+import SignupPage from "./routes/pages/SignupPage.jsx";
 
 const mainForm = () => {
   return (
