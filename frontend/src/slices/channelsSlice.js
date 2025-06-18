bf50e0850560587ba99aca9ee3321ef741268c45
AX <ax@MacBook-Pro-NS.local>
2025-06-19 01:32:40 +0300

    pre-final V 1.0

--- frontend/src/slices/channelsSlice.js
+++ frontend/src/slices/channelsSlice.js
@@ -1,14 +1,14 @@
 import { createSlice, createEntityAdapter } from "@reduxjs/toolkit";
 const channelsAdapter = createEntityAdapter();
 
-/* const initialState = channelsAdapter.getInitialState({
+const initialState = channelsAdapter.getInitialState({
   entities: {
-    1: { id: 1, name: 'general', removable: false },
-    2: { id: 2, name: 'random', removable: false },
+    1: { id: 1, name: "general", removable: false },
+    2: { id: 2, name: "random", removable: false },
   },
-  ids: [1, 2] // Важно: ids должны соответствовать ключам в entities
-}); */
-const initialState = channelsAdapter.getInitialState();
+  ids: [1, 2], // Важно: ids должны соответствовать ключам в entities
+});
+/* const initialState = channelsAdapter.getInitialState(); */
 
 const channelsSlice = createSlice({
   name: "channels",
